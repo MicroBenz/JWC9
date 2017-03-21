2f641635f7b67fdcbd2976a7c1407b19e035a62b
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-03-21 20:40:39 +0700

    Animation is work...but all cloud has the same animation

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -10,6 +10,13 @@ JWC#9
 
 @section('content')
 <section class="landing">
-    
+    <div class="logo-container">
+        <img src="{{URL::asset('img/cloud/left-top.png')}}" class="floating-cloud left-top">
+        <img src="{{URL::asset('img/cloud/left-mid.png')}}" class="floating-cloud left-mid">
+        <img src="{{URL::asset('img/cloud/left-bottom.png')}}" class="floating-cloud left-bottom">
+        <img src="{{URL::asset('img/cloud/right-top.png')}}" class="floating-cloud right-top">
+        <img src="{{URL::asset('img/cloud/right-mid.png')}}" class="floating-cloud right-mid">
+        <img src="{{URL::asset('img/cloud/right-bottom.png')}}" class="floating-cloud right-bottom">  
+    </div>
 </section>
 @endsection
