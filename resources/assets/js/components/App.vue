d1e1bde31e1405877dce0e321aab31ad70d57b8a
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-03-28 10:35:45 +0700

    Adding social button

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <apps-nav></apps-nav>
+    <social-group-button></social-group-button>
     <router-link to="/">To Landing</router-link>
-    <router-link to="/register">To Register</router-link>    
+    <router-link to="/register">To Register</router-link>
     <router-view></router-view>
   </div>
 </template>
 <script>
 import AppsNav from './nav/Nav';
+import SocialGroupButton from './social-button/SocialGroupButton';
+
 export default {
   components: {
-    AppsNav
+    AppsNav,
+    SocialGroupButton
   }
 }
 </script>
---
+++ resources/assets/js/components/social-button/SocialGroupButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="social-button-group">
+    <social-button icon-class="fa fa-facebook" link="facebook.com" bg-color="#337ab7"></social-button>
+    <social-button icon-class="fa fa-twitter" link="twitter.com" bg-color="#2AAAE0"></social-button>
+  </div>
+</template>
+<script>
+  import SocialButton from './SocialButton.vue';
+
+  export default {
+    components: {
+      SocialButton
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .social-button-group {
+    position: fixed;
+    right: 0;
+    top: 40%;
+  }
+</style>
