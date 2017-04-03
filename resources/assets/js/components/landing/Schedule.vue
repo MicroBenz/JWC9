b6a01eec56dfc31b98e19ad3710b8d3dcd1fc16b
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-04-03 18:22:13 +0700

    Schedule: Adding schedule

--- resources/assets/js/components/landing/Schedule.vue
+++ resources/assets/js/components/landing/Schedule.vue
@@ -1,28 +1,37 @@
 <template>
-  <div>
-    <h1>กำหนดการ</h1>
-    <div class="col-md-3 col-sm-6">
-      <div class="schedule-item">
-        <h3>รับสมัคร</h3>
-        <p>7 เมษายน วันส่งงาน...</p>
-      </div>
+  <div class="schedule">
+    <div class="schedule-title">
+      <h1 class="title-eng">Schedule</h1>
+      <h1 class="title-thai">กำหนดการ</h1>
     </div>
-    <div class="col-md-3 col-sm-6">
-      <div class="schedule-item">
-        <h3>ประกาศผลเข้ารอบ</h3>
-        <p>7 เมษายน วันส่งงาน...</p>
+    <div class="container">
+      <div class="col-md-3 col-sm-6">
+        <div class="schedule-item">
+          <img class="" src="/img/landing/schedule/1.png">
+          <h3>รับสมัคร</h3>
+          <p>7 เมษายน วันส่งงาน...</p>
+        </div>
       </div>
-    </div>
-    <div class="col-md-3 col-sm-6">
-      <div class="schedule-item">
-        <h3>สัมภาษณ์</h3>
-        <p>7 เมษายน วันส่งงาน...</p>
+      <div class="col-md-3 col-sm-6">
+        <div class="schedule-item">
+          <img src="/img/landing/schedule/2.png">          
+          <h3>ประกาศผลเข้ารอบ</h3>
+          <p>7 เมษายน วันส่งงาน...</p>
+        </div>
       </div>
-    </div>
-    <div class="col-md-3 col-sm-6">
-      <div class="schedule-item">
-        <h3>ประกาศผลตัวจริงและสำรอง</h3>
-        <p>7 เมษายน วันส่งงาน...</p>
+      <div class="col-md-3 col-sm-6">
+        <div class="schedule-item">
+          <img src="/img/landing/schedule/3.png">          
+          <h3>สัมภาษณ์</h3>
+          <p>7 เมษายน วันส่งงาน...</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class="schedule-item">
+          <img src="/img/landing/schedule/4.png">          
+          <h3>ประกาศผลตัวจริงและสำรอง</h3>
+          <p>7 เมษายน วันส่งงาน...</p>
+        </div>
       </div>
     </div>
   </div>
@@ -34,8 +43,20 @@
   }
 </script>
 
-<style scoped>
-  h3 {
-    margin: 0;
+<style lang="scss" scoped>
+@import '../../../sass/_variables.scss';
+
+.schedule {
+  @include background('/img/bg/brown.png');
+  .schedule-title {
+    .title-eng {
+      text-transform: uppercase;
+    }
   }
+  .schedule-item {
+    img {
+      width: 90%;
+    }
+  }
+}
 </style>
