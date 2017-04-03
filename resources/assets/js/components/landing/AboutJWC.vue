45da7cfa4bffd1ecda9f83ffa4d71f81a9c3be1e
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-04-03 17:51:38 +0700

    Adding about jwc

--- resources/assets/js/components/landing/AboutJWC.vue
+++ resources/assets/js/components/landing/AboutJWC.vue
@@ -1,11 +1,12 @@
 <template>
-<div>
-  <div class="col-md-5">
-    <h1>Picture Here</h1>
+<div class="about-jwc">
+  <div class="container">
+    <div class="col-md-5">
+      <img class="about-img" src="/img/landing/about/about.png">
+    </div>
+    <div class="col-md-7 about-jwc-text">
+      <p>ค่ายที่จะพาน้องๆ ม.ปลาย ค้นหาตัวตนที่ใช่ กับ 3 สาขาสำคัญในโลกการทำงานดิจิทัล ทั้ง <span class="role-text">Marketing, Content Creator</span> และ <span class="role-text">Design</span> พร้อมเรียนรู้เทคนิคสร้างสรรค์ไอเดียให้บรรเจิดผ่านกิจกรรมสุดว๊าวที่มีแค่ค่ายนี้เท่านั้น โดยภายในค่ายจะมี Workshop พิเศษจากพี่ๆ สุดเทพทั้งสามสาขา เพื่อขนอาวุธ ระดมไอเดีย สร้างโปรเจคนวัตกรรมของโลกยุค 2017</p>
   </div>
-  <div class="col-md-7 about-jwc">
-    <h1>JWC คืออะไร?</h1>
-    <p>ค่ายที่จะพาน้องๆ ม.ปลาย ค้นหาตัวตนที่ใช่ กับ 3 สาขาสำคัญในโลกการทำงานดิจิทัล ทั้ง Marketing, Content Creator และ Design พร้อมเรียนรู้เทคนิคสร้างสรรค์ไอเดียให้บรรเจิดผ่านกิจกรรมสุดว๊าวที่มีแค่ค่ายนี้เท่านั้น โดยภายในค่ายจะมี Workshop พิเศษจากพี่ๆ สุดเทพทั้งสามสาขา เพื่อขนอาวุธ ระดมไอเดีย สร้างโปรเจคนวัตกรรมของโลกยุค 2017</p>
   </div>
 </div>
 </template>
@@ -16,7 +17,22 @@ export default {
 }
 </script>
 <style lang="scss">
-  .about-jwc {
-    text-align: left;
+.about-jwc {
+  padding-top: 50px;
+  padding-bottom: 50px;
+  text-align: left;
+  background: url('/img/bg/blue.png') repeat center;
+  .about-img {
+    width: 100%;
   }
+  .about-jwc-text {
+    padding: 15px 24px;
+    border-radius: 15px;
+    background-color: #1e1e1e;
+    font-style: italic;
+    .role-text {
+      color: #fcc111;
+    }
+  }
+}
 </style>
