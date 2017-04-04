697963d4e857760fadab81dd2cb42343ed6adcdf
Natthasak Vechprasit <ve.natthasak_st@tni.ac.th>
2017-04-04 22:38:39 +0700

    Character bootstrap (HOT HEAD)

--- resources/assets/js/components/landing/role-selection/RoleSelection.vue
+++ resources/assets/js/components/landing/role-selection/RoleSelection.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="container">
-    <div class="col-md-4 about-role">
+    <div class="bg-character" style="transform: scale(0.8, 0.8)">
+  <div class="containe-fluidr">
+    <div class="col-md-3 about-role">
       <h1>Designer</h1>
       <p>Quis ad cillum cupidatat adipisicing. Magna veniam duis minim consectetur aliqua minim aute sit irure occaecat velit. Enim cupidatat nisi aliquip aute et elit ullamco excepteur voluptate dolor ea laborum magna. Quis est ea elit ut proident incididunt ullamco. Laboris ullamco commodo consectetur irure amet anim cillum magna aliquip Lorem. Officia reprehenderit ullamco eu anim labore occaecat amet culpa aute officia excepteur labore. Officia voluptate amet minim ut culpa nostrud quis commodo aliqua laborum ex dolor mollit.</p>
     </div>
-    <div class="col-md-4">
-      <h1>PIC</h1>
+    <div style="transform: scale(0.8, 0.8)" class="col-md-4 col-md-offset-1 presenter-stage">
+        <div style="background-image: url('img/characters/Light-Design.png" class="presenter-light">
+            <img class="presenter-body" src="/img/characters/Human_Design.png">
+        </div>
+        <div style="width: 300px; height: 80px; background: white; margin: 30px auto;">
+            Join
+        </div>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-3 col-md-offset-1 role-selector">
       <role-selector
         :role="roleConfig.designer.name"
         :description="roleConfig.designer.shortDescription"
@@ -25,6 +31,7 @@
       ></role-selector>
     </div>
   </div>
+    </div>
 </template>
 
 <script>
@@ -44,7 +51,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .about-role {
-    text-align: left;
-  }
+    .about-role {
+        text-align: left;
+    }
+    .bg-character{
+        background: url('/img/characters/bg-character.png');
+        background-size: cover;
+        background-position: center;
+        min-height: 900px;
+    }
+    .role-selector{
+        margin-top: 120px;
+    }
+    .presenter-stage{
+        position: relative;
+    }
+    .presenter-light{
+        background-size: 100% 100vh;
+        background-position: center;
+        background-repeat: no-repeat;
+        height: 80%;
+    }
+    .presenter-body{
+        max-width: 60%;
+        min-height: 755px;
+        height: 100vh;
+        padding-top: 60px;
+        padding-bottom: 15px;
+    }
 </style>
--- resources/assets/js/configs/role.js
+++ resources/assets/js/configs/role.js
@@ -3,18 +3,18 @@ export default {
     name: 'Content',
     description: 'Nostrud ut et culpa enim ea est ipsum nisi qui eu id exercitation dolore enim. Officia anim qui labore minim proident cillum irure reprehenderit. In commodo occaecat occaecat veniam sint non aliquip cupidatat exercitation occaecat anim quis dolor duis. Ut id culpa anim proident reprehenderit ut consectetur.',
     shortDescription: 'abcdeddssdjksdjslkdjsdk',
-    imgSrc: '/img/characters/content.png'
+    imgSrc: '/img/characters/R-ContentButton.png'
   },
   designer: {
     name: 'Designer',
     description: 'Elit reprehenderit nulla cupidatat minim ipsum. Non exercitation aute esse anim veniam Lorem non aute. Anim ipsum officia est laborum dolor mollit nisi minim laborum officia fugiat.',
     shortDescription: 'abcdeddssdjksdjslkdjsdk',
-    imgSrc: '/img/characters/designer.png'
+    imgSrc: '/img/characters/R-DesignButton.png'
   },
   marketing: {
     name: 'Marketing',
     description: 'Culpa exercitation deserunt id cillum ut veniam laboris aliqua. Occaecat labore dolor voluptate cillum tempor ut irure Lorem exercitation labore reprehenderit. Lorem nisi ex esse aliqua est ipsum ullamco laboris eu officia incididunt incididunt. Eiusmod magna consectetur adipisicing et minim proident irure magna mollit occaecat nostrud. Adipisicing irure nisi occaecat ex.',
     shortDescription: 'abcdeddssdjksdjslkdjsdk',
-    imgSrc: '/img/characters/marketing.png'
+    imgSrc: '/img/characters/R-MarketingButton.png'
   }
 }
