b2e4f8ea24947852edd776d7097de930d109bdfe
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-04-07 20:26:32 +0700

    Dynamically active class on top progress

--- resources/assets/js/pages/Register.page.vue
+++ resources/assets/js/pages/Register.page.vue
@@ -4,31 +4,41 @@
       <div class="row bs-wizard" style="border-bottom:0;">
           <div class="col-xs-1">
           </div>
-          <div class="col-xs-2 bs-wizard-step complete">
-            <div class="text-center bs-wizard-stepnum">Step 1 {{test}}</div>
+          <div class="col-xs-2 bs-wizard-step"
+            v-bind:class="{ 'complete': currentStep > 1, 'active': currentStep === 1, 'disabled': currentStep < 1 }"
+          >
+            <div class="text-center bs-wizard-stepnum">Step 1 {{currentStep}}</div>
             <div class="progress"><div class="progress-bar"></div></div>
             <a href="#" class="bs-wizard-dot"></a>
           </div>
           
-          <div class="col-xs-2 bs-wizard-step complete"><!-- complete -->
+          <div class="col-xs-2 bs-wizard-step"
+            v-bind:class="{ 'complete': currentStep > 2, 'active': currentStep === 2, 'disabled': currentStep < 2 }"
+          ><!-- complete -->
             <div class="text-center bs-wizard-stepnum">Step 2</div>
             <div class="progress"><div class="progress-bar"></div></div>
             <a href="#" class="bs-wizard-dot"></a>
           </div>
           
-          <div class="col-xs-2 bs-wizard-step active"><!-- complete -->
+          <div class="col-xs-2 bs-wizard-step"
+            v-bind:class="{ 'complete': currentStep > 3, 'active': currentStep === 3, 'disabled': currentStep < 3 }"
+          ><!-- complete -->
             <div class="text-center bs-wizard-stepnum">Step 3</div>
             <div class="progress"><div class="progress-bar"></div></div>
             <a href="#" class="bs-wizard-dot"></a>
           </div>
           
-          <div class="col-xs-2 bs-wizard-step disabled"><!-- active -->
+          <div class="col-xs-2 bs-wizard-step"
+            v-bind:class="{ 'complete': currentStep > 4, 'active': currentStep === 4, 'disabled': currentStep < 4 }"
+          ><!-- active -->
             <div class="text-center bs-wizard-stepnum">Step 4</div>
             <div class="progress"><div class="progress-bar"></div></div>
             <a href="#" class="bs-wizard-dot"></a>
           </div>
-          <div class="col-xs-2 bs-wizard-step disabled"><!-- active -->
-            <div class="text-center bs-wizard-stepnum">Step 4</div>
+          <div class="col-xs-2 bs-wizard-step"
+            v-bind:class="{ 'complete': currentStep > 5, 'active': currentStep === 5, 'disabled': currentStep < 5 }"
+          ><!-- active -->
+            <div class="text-center bs-wizard-stepnum">Step 5</div>
             <div class="progress"><div class="progress-bar"></div></div>
             <a href="#" class="bs-wizard-dot"></a>
           </div>
@@ -45,15 +55,20 @@
 
 <script>
   export default {
-    data: function(){
-      return {
-        currentStep: 'Profile'
-      }
-    },
     computed: {
         currentStep() {
-            console.log(this.$route.path);
-            return this.$route.path;
+            // console.log(this.$route.path);
+            const routeArr = this.$route.path.split('/');
+            const currentStep = routeArr[routeArr.length - 1].split('step')[1];
+            console.log(currentStep[0]);
+            return Number(currentStep[0]);
+        }
+    },
+    methods: {
+        getClass(step) {
+            const routeArr = this.$route.path.split('/');
+            const currentStep = routeArr[routeArr.length - 1].split('step')[1];
+            console.log(currentStep);
         }
     }
   }
@@ -120,6 +135,7 @@
     width: 0px;
     box-shadow: none;
     background: #fbe8aa;
+    transition: none;
 }
 .bs-wizard > .bs-wizard-step.complete > .progress > .progress-bar {
     width: 100%;
