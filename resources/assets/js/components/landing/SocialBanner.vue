a0c17ec580acc4771bea5374043b318e46d11f36
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-04-07 22:35:43 +0700

    Responsiveeeeeee

--- resources/assets/js/components/landing/SocialBanner.vue
+++ resources/assets/js/components/landing/SocialBanner.vue
@@ -1,13 +1,12 @@
 <template>
 <div class="social-banner">
-  <h1 class="topic-header" style="font-style: italic; font-weight: bold;
-    text-transform: uppercase; font-size: 88px;
-    line-height: 68px; color: #fbc80b">Contact us</h1>
+  <h1 class="topic-header">Contact us</h1>
   <h4 class="topic-subheader">ติดต่อเรา</h4>
   <br>
   <div class="container">
     <div class="col-md-6 fb-wrapper">
-      <div class="fb-page" style="width: 100%;" data-href="https://www.facebook.com/jwcth/" data-tabs="timeline" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/jwcth/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/jwcth/">Junior Webmaster Camp</a></blockquote></div>
+      <div v-if="isMobile" class="fb-page" data-href="https://www.facebook.com/jwcth/" data-tabs="timeline" data-width="240" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/jwcth/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/jwcth/">Junior Webmaster Camp</a></blockquote></div>
+      <div v-if="!isMobile" class="fb-page" data-href="https://www.facebook.com/jwcth/" data-tabs="timeline" data-width="500" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/jwcth/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/jwcth/">Junior Webmaster Camp</a></blockquote></div>
     </div>
     <div class="col-md-6">
       <a class="twitter-timeline" data-height="500" href="https://twitter.com/ywcth">Tweets by ywcth</a>
@@ -17,9 +16,16 @@
 </template>
 
 <script>
-  export default {
-    
+export default {
+  data(){
+    return {
+      isMobile: window.screen.width <= 320
+    };
+  },
+  mounted() {
+    console.log('did mount', window.screen.width < 320);
   }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -27,10 +33,11 @@
 
 .topic-header {
   font-style: italic;
+  font-weight: bold;
   text-transform: uppercase;
   font-size: 88px;
   line-height: 68px;
-  color: '#fbc80b';
+  color: #fbc80b;
   @media(max-width: 768px) {
     font-size: 64px;
     line-height: 45px;
@@ -62,6 +69,11 @@
 }
 .fb-page{
   margin-bottom: 10%;
+}
+.fb-page, 
+.fb-page span, 
+.fb-page span iframe[style] { 
+    width: 100% !important; 
 }
 .fb-wrapper {
   @media(max-width: 768px) {
