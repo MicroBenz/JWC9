1da7224f6be4f513654eb3d7743636f61143df2b
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-04-08 21:34:14 +0700

    Adding 404 page

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,7 +4,9 @@ import vueScrollTo from 'vue-scroll-to';
 
 import Landing from './pages/Landing.page.vue';
 import Register from './pages/Register.page.vue';
-import store from './store/index';
+import PageNotFound from './pages/404.page.vue';
+// import SocialGroupButton from './components/social-button/SocialGroupButton.vue';
+// import HamburgerMenu from './components/menu/HamburgerMenu.vue';
 import Profile from './components/registers/Profile.Register.vue';
 import Contact from './components/registers/Contact.Register.vue';
 import Other from './components/registers/Other.Register.vue';
@@ -12,7 +14,7 @@ import GeneralQuestion from './components/registers/GeneralQuestion.Register.vue
 import Designer from './components/registers/roles/Designer.Register.vue';
 import Content from './components/registers/roles/Content.Register.vue';
 import Marketer from './components/registers/roles/Marketing.Register.vue';
-
+import store from './store/index';
 // import 'loaders.css';
 
 Vue.use(VueRouter);
@@ -25,46 +27,49 @@ const router = new VueRouter({
       path: '/register',
       component: Register,
       children: [
-            {
-              path: '/',
-              component: Profile
-            },
-            {
-              path: 'step1',
-              component: Profile
-            },
-            {
-              path: 'step2',
-              component: Contact
-            },
-            {
-              path: 'step3',
-              component: Other
-            },
-            {
-              path: 'step4',
-              component: GeneralQuestion
-            },
-            {
-              path: 'step5-design',
-              component: Designer
-            },
-            {
-              path: 'step5-content',
-              component: Content
-            },
-            {
-              path: 'step5-marketing',
-              component: Marketer
-            }
-            ]
-       },
+          {
+            path: '/',
+            component: Profile
+          },
+          {
+            path: 'step1',
+            component: Profile
+          },
+          {
+            path: 'step2',
+            component: Contact
+          },
+          {
+            path: 'step3',
+            component: Other
+          },
+          {
+            path: 'step4',
+            component: GeneralQuestion
+          },
+          {
+            path: 'step5-design',
+            component: Designer
+          },
+          {
+            path: 'step5-content',
+            component: Content
+          },
+          {
+            path: 'step5-marketing',
+            component: Marketer
+          }
+          ]
+      },
+      { path: '*', component: PageNotFound }
   ],
   mode: 'history'
 })
 
 Vue.component('app', require('./components/App.vue'));
 Vue.component('loader', require('./components/Loader.vue'));
+Vue.component('apps-menu', require('./components/menu/HamburgerMenu.vue'));
+Vue.component('social-group-button', require('./components/social-button/SocialGroupButton.vue'));
 
 const app = new Vue({
   router,
