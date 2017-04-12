65fa76e1551055d2d5775d945193ad88a29ae5ad
Natthasak Vechprasit <ve.natthasak_st@tni.ac.th>
2017-04-13 00:07:11 +0700

    Update content

--- resources/assets/js/components/landing/SocialBanner.vue
+++ resources/assets/js/components/landing/SocialBanner.vue
@@ -9,7 +9,7 @@
       <div v-if="!isMobile" class="fb-page" data-href="https://www.facebook.com/jwcth/" data-tabs="timeline" data-width="500" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/jwcth/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/jwcth/">Junior Webmaster Camp</a></blockquote></div>
     </div>
     <div class="col-md-6">
-      <a class="twitter-timeline" data-height="500" href="https://twitter.com/ywcth">Tweets by ywcth</a>
+      <a class="twitter-timeline" data-height="500" href="https://twitter.com/jwcth">Tweets by ywcth</a>
     </div>
   </div>
 </div>
