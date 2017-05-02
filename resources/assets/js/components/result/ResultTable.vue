7ad0734fa421398dc9747e710ef89111fd59e8b3
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-05-02 11:02:28 +0700

    Styling a bit

--- resources/assets/js/components/result/ResultTable.vue
+++ resources/assets/js/components/result/ResultTable.vue
@@ -1,8 +1,13 @@
 <template>
 <div>
-  <h1>{{role}}</h1>
+  <h1 class="role">{{role}}</h1>
   <table class="table">
-    <thead>
+    <thead v-bind:class="[
+      'table-header',
+      role === 'Content' ? 'content-header' : '',
+      role === 'Designer' ? 'design-header' : '',      
+      role === 'Marketing' ? 'marketing-header' : '',
+    ]">
       <tr>
         <td>ID</td>
         <td>ชื่อ</td>
@@ -27,9 +32,30 @@ export default {
 </script>
 
 <style lang="scss">
+@import '../../../sass/_variables.scss';
+
+.role {
+  text-transform: uppercase;
+  font-size: 60px;
+  padding-bottom: 15px;
+}
 .table {
   background-color: white;
   color: black;
   font-size: 22px;
+  text-align: left;
+  .table-header {
+    font-weight: bold;
+    color: white;
+  }
+  .content-header {
+    background-color: $content-color;
+  }
+  .design-header {
+    background-color: $design-color;
+  }
+  .marketing-header {
+    background-color: $marketing-color;  
+  }
 }
 </style>
