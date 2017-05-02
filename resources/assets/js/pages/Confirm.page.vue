fecb51ce0f2c89bb84bc876fae1065361d43ded2
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-05-02 23:43:36 +0700

    Add confirm slip page

--- resources/assets/js/pages/Confirm.page.vue
+++ resources/assets/js/pages/Confirm.page.vue
@@ -1,8 +1,27 @@
 <template>
-<div class="confirm-container">
-  <h1>Confirm (Upload Slip)</h1>
-  <p>{{token}}</p>
-</div>
+  <div class="confirm-container">
+    <h1>ยืนยันสิทธิ์</h1>
+    <div class="camper-detail">
+      <h4><b>Ref ID:</b></h4>
+      <h4><b>ชื่อ-นามสกุล:</b> นายทดสอบ นามสกุลเทสสสสส</h4>
+      <h4><b>ยอดเงิน:</b> 200.11</h4>
+    </div>
+    <div>
+      <div class="slip-image">
+        <img v-if="img === ''" src="http://bulma.io/images/placeholders/128x128.png">
+        <img v-else :src="img">        
+      </div>
+      <div style="text-align:center;">
+          <input type="file" id="files" class="hidden" v-on:change="onFileChange" accept="image/*"/>
+          <label for="files">
+              <!--<img class="upload-btn" src="./upload.png">-->
+              <a class="btn-game">
+                  <div class="btn-game upload-btn">อัพโหลดหลักฐานการโอนเงิน</div>
+              </a>
+          </label>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,22 +30,97 @@ import { mapGetters } from 'vuex';
 export default {
   mounted() {
     if (this.$store.getters.accessToken === '') {
-      this.$router.push('/');
+      // this.$router.push('/');
     }
   },
+  data: () => ({
+    img: '',
+  }),
   computed: mapGetters({
     token: 'accessToken',
-  })
+  }),
+  methods: {
+    onFileChange(e) {
+      const files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createImage(files[0]);
+
+      // let formData = new FormData()
+      // formData.append('ProfilePicture', files[0])
+
+      // axios.post('/api/register/profilepicture', formData).then(function (res) {
+      //   console.log(res.data)
+      // })
+    },
+    createImage(file) {
+      const reader = new FileReader();
+      const vm = this;
+      reader.onload = function (e) {
+        const image = new Image();
+        image.onload = () => {
+          console.log('success', image.width, image.height);
+          console.log(file.size)
+          if (file.size <= 2100000) {
+            vm.img = e.target.result;
+          }
+          else {
+            alert('ขนาดไฟล์เกิน 2 MB')
+          }
+        }
+        image.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+  }
 }
 </script>
 
 <style lang="scss">
 @import '../../sass/_variables.scss';
 
+.upload-btn:hover {
+    margin-top: 0px;
+}
+.upload-btn {
+    font-size: 32px;
+    padding-left: 25px;
+    padding-right: 25px;
+    border-bottom: none;
+    max-width: 400px;
+}
+
 .confirm-container {
   @include background('/img/bg/brown.png');
   text-align: center;
   padding-top: 50px;
   padding-bottom: 50px;
+  .camper-detail {
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+  .slip-image {
+    height: 350px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    img {
+      height: 100%;
+    }
+  }
+  .confirm-btn {
+    width: 200px;
+    background: #3364a7;
+    color: white;
+    border-radius: 30px;
+    border-bottom: 5px solid #1a40a7;
+    margin: auto;
+    margin-top: 10px;
+    margin-bottom: 20px;
+    cursor: pointer;
+    &:hover {
+      margin-top: 13px;
+      border-bottom: 3px solid #2455a7;
+    }
+  }
 }
 </style>
