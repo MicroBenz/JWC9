5b565a93558e96e58fb7fd37dd76f3d21ab70a45
Thanyakorn Daengkasem <kornny24159@gmail.com>
2017-05-07 01:07:25 +0700

    change migrate table announcements

---
+++ database/migrations/2017_05_07_010044_add_AnnounceID_to_announcements_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAnnounceIDToAnnouncementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('announcements', function (Blueprint $table) {
+            $table->increments('AnnounceID')->first();
+            $table->dropPrimary('CamperID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // 
+    }
+}
