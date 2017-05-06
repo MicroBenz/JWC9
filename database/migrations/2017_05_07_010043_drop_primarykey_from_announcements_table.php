4cf0d162c23442016646054270dbcf840f2ad7e1
Thanyakorn Daengkasem <kornny24159@gmail.com>
2017-05-07 02:01:14 +0700

    drop and change migrate

--- database/migrations/2017_05_07_010043_drop_primarykey_from_announcements_table.php
+++ database/migrations/2017_05_07_010043_drop_primarykey_from_announcements_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddAnnounceIDToAnnouncementsTable extends Migration
+class DropPrimarykeyFromAnnouncementsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddAnnounceIDToAnnouncementsTable extends Migration
     public function up()
     {
         Schema::table('announcements', function (Blueprint $table) {
-            $table->increments('AnnounceID')->first();
+            $table->dropPrimary('CamperID');
         });
     }
 
